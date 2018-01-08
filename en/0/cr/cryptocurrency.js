1ebb16cb5369ef8ab117cf23f0a748124635ecde
microstorage <microstorage@users.noreply.github.com>
2018-01-08 20:43:18 +0100

    My commit

---
+++ en/0/cr/cryptocurrency.js
@@ -0,0 +1,3 @@
+Gr = [
+"Video - Bitcoin can only process 7 transactions per second, how can we fix this?|Bitcoin's Lightning Network, simply explained!|2018/01/video-bitcoin-can-only-process-7-transactions-per-second-how-can-we-fix-this|YT_rrr_zPmEiME",
+];
---
+++ it/0/te/telescopio.js
@@ -0,0 +1,3 @@
+Gr = [
+"Video - L'Hubble Ultra Deep Field in 3D|Sicuro che non c'è proprio niente laggiù?|2018/01/video-l-hubble-ultra-deep-field-in-3d|YT_oAVjF_7ensg",
+];
