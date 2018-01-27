c84d6632bafdc480dab1f1e0158120b147fe405b
microstorage <microstorage@users.noreply.github.com>
2018-01-27 12:59:30 +0100

    My commit

--- it/0/index.js
+++ it/0/index.js
@@ -1,3 +1,4 @@
 Gr = [
+"Video - Cercare lavoro stanca|La selezione personale in stile Milanese Imbruttito|2018/01/video-cercare-lavoro-stanca|YT_OKJLlJyWNTc",
 "Video - L'Hubble Ultra Deep Field in 3D|Sicuro che non c'è proprio niente laggiù?|2018/01/video-l-hubble-ultra-deep-field-in-3d|YT_oAVjF_7ensg",
 ];
---
+++ it/0/la/lavoro.js
@@ -0,0 +1,3 @@
+Gr = [
+"Video - Cercare lavoro stanca|La selezione personale in stile Milanese Imbruttito|2018/01/video-cercare-lavoro-stanca|YT_OKJLlJyWNTc",
+];
