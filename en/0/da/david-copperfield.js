1a2d42259906e2ecf206a52ef74d7cda58334512
microstorage <microstorage@users.noreply.github.com>
2018-01-27 18:09:25 +0100

    My commit

---
+++ en/0/da/david-copperfield.js
@@ -0,0 +1,3 @@
+Gr = [
+"Video - CES 2018 strangest booths|iJustine and David Copperfield together?|2018/01/video-ces-2018-strangest-booths|YT_mbAPZq5HFAE",
+];
--- en/0/index.js
+++ en/0/index.js
@@ -1,3 +1,4 @@
 Gr = [
+"Video - CES 2018 strangest booths|iJustine and David Copperfield together?|2018/01/video-ces-2018-strangest-booths|YT_mbAPZq5HFAE",
 "Video - Bitcoin can only process 7 transactions per second, how can we fix this?|Bitcoin's Lightning Network, simply explained!|2018/01/video-bitcoin-can-only-process-7-transactions-per-second-how-can-we-fix-this|YT_rrr_zPmEiME",
 ];
