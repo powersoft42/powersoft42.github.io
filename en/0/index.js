7025307d21a25e20b6ea8f4bd59ebcf445632231
microstorage <microstorage@users.noreply.github.com>
2018-02-05 16:11:10 +0100

    My commit

--- en/0/index.js
+++ en/0/index.js
@@ -1,4 +1,5 @@
 Gr = [
+"Video - Bear and Goose and Ursula|Also some stolen bits from The Last Question, by Isaac Asimov|2018/02/video-bear-and-goose-and-ursula|YT_3N5lgUgAQ-g",
 "Video - What happens in your head when you speak two languages?|Tia Taylor and her struggles with her being multilingual|2018/01/video-what-happens-in-your-head-when-you-speak-two-languages|YT_Hug5VbsqOqw",
 "Video - CES 2018 strangest booths|iJustine and David Copperfield together?|2018/01/video-ces-2018-strangest-booths|YT_mbAPZq5HFAE",
 "Video - Bitcoin can only process 7 transactions per second, how can we fix this?|Bitcoin's Lightning Network, simply explained!|2018/01/video-bitcoin-can-only-process-7-transactions-per-second-how-can-we-fix-this|YT_rrr_zPmEiME",
---
+++ en/0/yo/youtube.js
@@ -0,0 +1,3 @@
+Gr = [
+"Video - Bear and Goose and Ursula|Also some stolen bits from The Last Question, by Isaac Asimov|2018/02/video-bear-and-goose-and-ursula|YT_3N5lgUgAQ-g",
+];
