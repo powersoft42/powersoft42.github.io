4e2352209ed24bed2ba6b19498a36031bb17390f
microstorage <microstorage@users.noreply.github.com>
2018-02-10 14:24:05 +0100

    My commit

--- es/0/index.js
+++ es/0/index.js
@@ -1,4 +1,5 @@
 Gr = [
+"Pantalones para Mujer, los 10 mejores: febrero 2018|El top 10 de los Pantalones para Mujer: febrero 2018|2018/02/pantalones-para-mujer-los-10-mejores-febrero-2018|AZ_412EDSOOmvL",
 "Las 10 Minipicadoras más valoradas del momento: febrero 2018|Las 10 más valoradas Minipicadoras que puedes comprar ahora mismo|2018/02/las-10-minipicadoras-mas-valoradas-del-momento-febrero-2018|AZ_31+KvgsdiDL",
 "Robots de Cocina, los 10 más vendidos: febrero 2018|La lista con los 10 Robots de Cocina mejores a febrero 2018|2018/02/robots-de-cocina-los-10-mas-vendidos-febrero-2018|AZ_41sPe7xByWL",
 "Cargadores de Batería para Coche, los 10 más valorados del mercado: febrero 2018|Los 10 mejor valorados Cargadores de Batería para Coche del mercado|2018/02/cargadores-de-bateria-para-coche-los-10-mas-valorados-del-mercado-febrero-2018|AZ_51-PY3-xfqL",
---
+++ es/0/pa/pantalones-elasticos.js
@@ -0,0 +1,3 @@
+Gr = [
+"Pantalones para Mujer, los 10 mejores: febrero 2018|El top 10 de los Pantalones para Mujer: febrero 2018|2018/02/pantalones-para-mujer-los-10-mejores-febrero-2018|AZ_412EDSOOmvL",
+];
