3732e6e81a050fc69bcbca41c5d8fb0549b5ca6a
microstorage <microstorage@users.noreply.github.com>
2018-02-10 14:52:21 +0100

    My commit

--- it/0/index.js
+++ it/0/index.js
@@ -1,4 +1,5 @@
 Gr = [
+"La top 10 delle Macchine del Pane: febbraio 2018|Le 10 Macchine del Pane più interessanti da valutare in commercio ora!|2018/02/la-top-10-delle-macchine-del-pane-febbraio-2018|AZ_41Zyk4pYfwL",
 "Mouse: i 10 migliori a febbraio 2018|La top 10 dei migliori Mouse subito disponibili: febbraio 2018|2018/02/mouse-i-10-migliori-a-febbraio-2018|AZ_51fAE77EpFL",
 "Video - Cercare lavoro stanca|La selezione personale in stile Milanese Imbruttito|2018/01/video-cercare-lavoro-stanca|YT_OKJLlJyWNTc",
 "Video - L'Hubble Ultra Deep Field in 3D|Sicuro che non c'è proprio niente laggiù?|2018/01/video-l-hubble-ultra-deep-field-in-3d|YT_oAVjF_7ensg",
---
+++ it/0/mo/moulinex.js
@@ -0,0 +1,3 @@
+Gr = [
+"La top 10 delle Macchine del Pane: febbraio 2018|Le 10 Macchine del Pane più interessanti da valutare in commercio ora!|2018/02/la-top-10-delle-macchine-del-pane-febbraio-2018|AZ_41Zyk4pYfwL",
+];
