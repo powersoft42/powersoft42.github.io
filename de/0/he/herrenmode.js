11ed305ff1aaecf5a18504f70f395a5cbd335c1f
microstorage <microstorage@users.noreply.github.com>
2018-02-10 15:08:40 +0100

    My commit

--- de/0/he/herrenmode.js
+++ de/0/he/herrenmode.js
@@ -1,4 +1,3 @@
 Gr = [
 "Die 10 besten Kapuzenpullover für Herren: Februar 2018|Top 10: die 10 besten in Kapuzenpullover für Herren, Februar 2018|2018/02/die-10-besten-kapuzenpullover-fur-herren-februar-2018|AZ_410RR48qAQL",
-"Top 10 Bestseller in Herrendüfte: Februar 2018|Top 10: die besten in Herrendüfte, Februar 2018|2018/02/top-10-bestseller-in-herrendufte-februar-2018|AZ_41P32nrp7NL",
 ];
