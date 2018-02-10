bfcb1ac565f0e9f549ef22fe846ae8ebb865399a
microstorage <microstorage@users.noreply.github.com>
2018-02-10 14:39:25 +0100

    My commit

---
+++ fr/0/el/electrostimulateur.js
@@ -0,0 +1,3 @@
+Gr = [
+"Electrostimulateurs, les 10 meilleurs: février 2018|Les 10 meilleurs Electrostimulateurs du moment: février 2018|2018/02/electrostimulateurs-les-10-meilleurs-fevrier-2018|AZ_510RfOYwzNL",
+];
--- fr/0/index.js
+++ fr/0/index.js
@@ -1,3 +1,4 @@
 Gr = [
+"Electrostimulateurs, les 10 meilleurs: février 2018|Les 10 meilleurs Electrostimulateurs du moment: février 2018|2018/02/electrostimulateurs-les-10-meilleurs-fevrier-2018|AZ_510RfOYwzNL",
 "Les 10 meilleurs Vélos Elliptiques du moment: février 2018|Le top 10 des meilleurs Vélos Elliptiques du moment: février 2018|2018/02/les-10-meilleurs-velos-elliptiques-du-moment-fevrier-2018|AZ_412L2TKac2L",
 ];
