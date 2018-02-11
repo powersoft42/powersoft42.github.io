ebf679139ded4e66306c5c8b9adbbea1d49fc74f
microstorage <microstorage@users.noreply.github.com>
2018-02-11 15:44:24 +0100

    My commit

--- it/0/cu/cucina-vegana.js
+++ it/0/cu/cucina-vegana.js
@@ -1,4 +1,3 @@
 Gr = [
 "Video - Alla scoperta dei Vegani con Alberto Angela?|Ma sarà proprio lui? Ehi ma c'è anche il babbo! Grandi!|2018/02/video-alla-scoperta-dei-vegani-con-alberto-angela|YT_Vdvdivj9HkU",
-"La top 10 delle Macchine del Pane: febbraio 2018|Le 10 Macchine del Pane più interessanti da valutare in commercio ora!|2018/02/la-top-10-delle-macchine-del-pane-febbraio-2018|AZ_41Zyk4pYfwL",
 ];
--- it/0/vi/video-divertenti.js
+++ it/0/vi/video-divertenti.js
@@ -1,3 +1,4 @@
 Gr = [
+"Video - Alla scoperta dei Vegani con Alberto Angela?|Ma sarà proprio lui? Ehi ma c'è anche il babbo! Grandi!|2018/02/video-alla-scoperta-dei-vegani-con-alberto-angela|YT_Vdvdivj9HkU",
 "Video - Cercare lavoro stanca|La selezione personale in stile Milanese Imbruttito|2018/01/video-cercare-lavoro-stanca|YT_OKJLlJyWNTc",
 ];
