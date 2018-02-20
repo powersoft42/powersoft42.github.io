17ff98ca698e3be9c2b763b44993ddfd91ae9765
microstorage <microstorage@users.noreply.github.com>
2018-02-20 15:53:25 +0100

    My commit

--- it/0/index.js
+++ it/0/index.js
@@ -1,4 +1,5 @@
 Gr = [
+"La top 10 dei migliori Binocoli: febbraio 2018|I 10 Binocoli più interessanti da valutare: la classifica|2018/02/la-top-10-dei-migliori-binocoli-febbraio-2018|AZ_41zW1mlAAQL",
 "Video - Alla scoperta dei Vegani con Alberto Angela?|Ma sarà proprio lui? Ehi ma c'è anche il babbo! Grandi!|2018/02/video-alla-scoperta-dei-vegani-con-alberto-angela|YT_Vdvdivj9HkU",
 "La top 10 delle Macchine del Pane: febbraio 2018|Le 10 Macchine del Pane più interessanti da valutare in commercio ora!|2018/02/la-top-10-delle-macchine-del-pane-febbraio-2018|AZ_41Zyk4pYfwL",
 "Mouse: i 10 migliori a febbraio 2018|La top 10 dei migliori Mouse subito disponibili: febbraio 2018|2018/02/mouse-i-10-migliori-a-febbraio-2018|AZ_51fAE77EpFL",
--- it/0/na/natura.js
+++ it/0/na/natura.js
@@ -1,4 +1,3 @@
 Gr = [
 "La top 10 dei migliori Binocoli: febbraio 2018|I 10 Binocoli più interessanti da valutare: la classifica|2018/02/la-top-10-dei-migliori-binocoli-febbraio-2018|AZ_41zW1mlAAQL",
-"Video - L'Hubble Ultra Deep Field in 3D|Sicuro che non c'è proprio niente laggiù?|2018/01/video-l-hubble-ultra-deep-field-in-3d|YT_oAVjF_7ensg",
 ];
