218bdc805488aba8b13fefee220942444234b75f
microstorage <microstorage@users.noreply.github.com>
2018-02-20 15:41:39 +0100

    My commit

--- es/0/el/electronica.js
+++ es/0/el/electronica.js
@@ -1,3 +1,4 @@
 Gr = [
+"Top 10 Ventiladores para Ventanas a febrero 2018|La lista con los 10 Ventiladores para Ventanas mejores a febrero 2018|2018/02/top-10-ventiladores-para-ventanas-a-febrero-2018|AZ_51aHYpkbbqL",
 "Cargadores de Batería para Coche, los 10 más valorados del mercado: febrero 2018|Los 10 mejor valorados Cargadores de Batería para Coche del mercado|2018/02/cargadores-de-bateria-para-coche-los-10-mas-valorados-del-mercado-febrero-2018|AZ_51-PY3-xfqL",
 ];
--- es/0/ve/ventilacion.js
+++ es/0/ve/ventilacion.js
@@ -1,4 +1,3 @@
 Gr = [
 "Top 10 Ventiladores para Ventanas a febrero 2018|La lista con los 10 Ventiladores para Ventanas mejores a febrero 2018|2018/02/top-10-ventiladores-para-ventanas-a-febrero-2018|AZ_51aHYpkbbqL",
-"Los 9 Congeladores más vendidos del mercado: febrero 2018|Los 9 más valorados Congeladores del momento: febrero 2018|2018/02/los-9-congeladores-mas-vendidos-del-mercado-febrero-2018|AZ_21Mx3yNV1aL",
 ];
