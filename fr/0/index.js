3a39c1510a2c8f0e00d1dc2be5ad16923660a700
microstorage <microstorage@users.noreply.github.com>
2018-02-20 15:47:03 +0100

    My commit

--- fr/0/index.js
+++ fr/0/index.js
@@ -1,4 +1,5 @@
 Gr = [
+"Top 10 des meilleurs Ventilateurs Colonne de février 2018|Guide d'achat: les 10 meilleurs Ventilateurs Colonne du moment|2018/02/top-10-des-meilleurs-ventilateurs-colonne-de-fevrier-2018|AZ_31syhMT1qaL",
 "Electrostimulateurs, les 10 meilleurs: février 2018|Les 10 meilleurs Electrostimulateurs du moment: février 2018|2018/02/electrostimulateurs-les-10-meilleurs-fevrier-2018|AZ_510RfOYwzNL",
 "Les 10 meilleurs Vélos Elliptiques du moment: février 2018|Le top 10 des meilleurs Vélos Elliptiques du moment: février 2018|2018/02/les-10-meilleurs-velos-elliptiques-du-moment-fevrier-2018|AZ_412L2TKac2L",
 ];
---
+++ fr/0/te/temperature.js
@@ -0,0 +1,3 @@
+Gr = [
+"Top 10 des meilleurs Ventilateurs Colonne de février 2018|Guide d'achat: les 10 meilleurs Ventilateurs Colonne du moment|2018/02/top-10-des-meilleurs-ventilateurs-colonne-de-fevrier-2018|AZ_31syhMT1qaL",
+];
