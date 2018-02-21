83c262137a718a3dc639732c2fad72b810d0db7f
microstorage <microstorage@users.noreply.github.com>
2018-02-21 15:53:29 +0100

    My commit

--- en/0/de/desktop-pc.js
+++ en/0/de/desktop-pc.js
@@ -1,3 +1,4 @@
 Gr = [
+"Top 10 Desktop PCs of February 2018: buying guide|Top-rated 10 Desktop PCs available to buy right now: February 2018|2018/02/top-10-desktop-pcs-of-february-2018-buying-guide|AZ_51VaxxZYf8L",
 "10 best-selling Gaming PCs: February 2018|Our pick of the best 10 Gaming PCs of February 2018|2018/02/10-best-selling-gaming-pcs-february-2018|AZ_51qnN7mlLvL",
 ];
