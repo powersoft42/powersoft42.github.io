b574908ff7c819a200da69a2b0a0b0a067b721da
microstorage <microstorage@users.noreply.github.com>
2018-02-26 15:14:00 +0100

    My commit

--- it/0/al/alimentazione.js
+++ it/0/al/alimentazione.js
@@ -1,4 +1,5 @@
 Gr = [
+"Macchine da Caffè a Capsule: le 10 migliori a febbraio 2018|Le 10 Macchine da Caffè a Capsule che incontrano il maggiore gradimento|2018/02/macchine-da-caffe-a-capsule-le-10-migliori-a-febbraio-2018|AZ_41kpFIglQGL",
 "Video - Alla scoperta dei Vegani con Alberto Angela?|Ma sarà proprio lui? Ehi ma c'è anche il babbo! Grandi!|2018/02/video-alla-scoperta-dei-vegani-con-alberto-angela|YT_Vdvdivj9HkU",
 "La top 10 delle Macchine del Pane: febbraio 2018|Le 10 Macchine del Pane più interessanti da valutare in commercio ora!|2018/02/la-top-10-delle-macchine-del-pane-febbraio-2018|AZ_41Zyk4pYfwL",
 ];
--- it/0/co/colazione.js
+++ it/0/co/colazione.js
@@ -1,3 +1,4 @@
 Gr = [
+"Macchine da Caffè a Capsule: le 10 migliori a febbraio 2018|Le 10 Macchine da Caffè a Capsule che incontrano il maggiore gradimento|2018/02/macchine-da-caffe-a-capsule-le-10-migliori-a-febbraio-2018|AZ_41kpFIglQGL",
 "La top 10 delle Macchine del Pane: febbraio 2018|Le 10 Macchine del Pane più interessanti da valutare in commercio ora!|2018/02/la-top-10-delle-macchine-del-pane-febbraio-2018|AZ_41Zyk4pYfwL",
 ];
