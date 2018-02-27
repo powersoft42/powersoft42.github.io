2594a7bd9b0fd3ea2ab2ed4d1ed085fbce25e4f1
microstorage <microstorage@users.noreply.github.com>
2018-02-27 19:30:36 +0100

    My commit

--- de/0/index.js
+++ de/0/index.js
@@ -1,4 +1,5 @@
 Gr = [
+"Top 10 besten Nachttischlampen: Februar 2018|10 beliebtesten Artikel in Nachttischlampen im Vergleich: Februar 2018|2018/02/top-10-besten-nachttischlampen-februar-2018|AZ_31budpIL9iL",
 "Top 10: die Bestseller in Strickmützen für Damen, Februar 2018|Die 10 beliebtesten Artikel in Strickmützen für Damen: Februar 2018|2018/02/top-10-die-bestseller-in-strickmutzen-fur-damen-februar-2018|AZ_51rFCwtQJ8L",
 "Top 10: die Bestseller in Gaming Notebooks, Februar 2018|Die 10 Bestseller in Gaming Notebooks, Februar 2018|2018/02/top-10-die-bestseller-in-gaming-notebooks-februar-2018|AZ_41yHWKm0DlL",
 "Die 10 besten Kapuzenpullover für Herren: Februar 2018|Top 10: die 10 besten in Kapuzenpullover für Herren, Februar 2018|2018/02/die-10-besten-kapuzenpullover-fur-herren-februar-2018|AZ_410RR48qAQL",
---
+++ de/0/ti/tischlampen.js
@@ -0,0 +1,3 @@
+Gr = [
+"Top 10 besten Nachttischlampen: Februar 2018|10 beliebtesten Artikel in Nachttischlampen im Vergleich: Februar 2018|2018/02/top-10-besten-nachttischlampen-februar-2018|AZ_31budpIL9iL",
+];
