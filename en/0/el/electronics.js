bbd80d550b7286302cf9410cea14e7927a78cee2
microstorage <microstorage@users.noreply.github.com>
2018-02-27 19:35:50 +0100

    My commit

--- en/0/el/electronics.js
+++ en/0/el/electronics.js
@@ -1,4 +1,5 @@
 Gr = [
+"The very best 10 Hi-Fi Receivers: February 2018|The 10 best-selling Hi-Fi Receivers of February 2018|2018/02/the-very-best-10-hi-fi-receivers-february-2018|AZ_41sVrHBQIfL",
 "Top 10 Dj Mixers of February 2018|Our pick of the best 10 Dj Mixers: February 2018|2018/02/top-10-dj-mixers-of-february-2018|AZ_51faAw8-EiL",
 "Top 10 Desktop PCs of February 2018: buying guide|Top-rated 10 Desktop PCs available to buy right now: February 2018|2018/02/top-10-desktop-pcs-of-february-2018-buying-guide|AZ_51VaxxZYf8L",
 "10 best-selling Gaming PCs: February 2018|Our pick of the best 10 Gaming PCs of February 2018|2018/02/10-best-selling-gaming-pcs-february-2018|AZ_51qnN7mlLvL",
--- en/0/st/stereo.js
+++ en/0/st/stereo.js
@@ -1,3 +1,4 @@
 Gr = [
+"The very best 10 Hi-Fi Receivers: February 2018|The 10 best-selling Hi-Fi Receivers of February 2018|2018/02/the-very-best-10-hi-fi-receivers-february-2018|AZ_41sVrHBQIfL",
 "Top 10 Dj Mixers of February 2018|Our pick of the best 10 Dj Mixers: February 2018|2018/02/top-10-dj-mixers-of-february-2018|AZ_51faAw8-EiL",
 ];
