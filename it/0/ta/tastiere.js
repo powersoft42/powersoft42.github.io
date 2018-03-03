a4a14614e97e8095676c02b67a9c7e9ab5b44426
microstorage <microstorage@users.noreply.github.com>
2018-03-03 14:49:33 +0100

    My commit

--- it/0/ta/tastiere.js
+++ it/0/ta/tastiere.js
@@ -1,4 +1,3 @@
 Gr = [
 "Le 10 Tastiere PC migliori: marzo 2018|La top 10 delle migliori Tastiere PC sul mercato a marzo 2018|2018/03/le-10-tastiere-pc-migliori-marzo-2018|AZ_41wgHR31YbL",
-"Mouse: i 10 migliori a febbraio 2018|La top 10 dei migliori Mouse subito disponibili: febbraio 2018|2018/02/mouse-i-10-migliori-a-febbraio-2018|AZ_51fAE77EpFL",
 ];
--- it/0/uf/ufficio.js
+++ it/0/uf/ufficio.js
@@ -1,3 +1,4 @@
 Gr = [
+"Le 10 Tastiere PC migliori: marzo 2018|La top 10 delle migliori Tastiere PC sul mercato a marzo 2018|2018/03/le-10-tastiere-pc-migliori-marzo-2018|AZ_41wgHR31YbL",
 "Macchine da Caffè a Capsule: le 10 migliori a febbraio 2018|Le 10 Macchine da Caffè a Capsule che incontrano il maggiore gradimento|2018/02/macchine-da-caffe-a-capsule-le-10-migliori-a-febbraio-2018|AZ_41kpFIglQGL",
 ];
