3ff4499cfdf3b60da45f094709283e6d62f8d8d4
microstorage <microstorage@users.noreply.github.com>
2018-03-03 15:05:38 +0100

    My commit

--- en/0/index.js
+++ en/0/index.js
@@ -1,4 +1,5 @@
 Gr = [
+"Buying guide: top 10 Briefcases of March 2018|The best 10 Briefcases of March 2018: buying guide|2018/03/buying-guide-top-10-briefcases-of-march-2018|AZ_512FcoN4T7L",
 "The very best 10 Hi-Fi Receivers: February 2018|The 10 best-selling Hi-Fi Receivers of February 2018|2018/02/the-very-best-10-hi-fi-receivers-february-2018|AZ_41sVrHBQIfL",
 "Top 10 Dj Mixers of February 2018|Our pick of the best 10 Dj Mixers: February 2018|2018/02/top-10-dj-mixers-of-february-2018|AZ_51faAw8-EiL",
 "Top 10 Desktop PCs of February 2018: buying guide|Top-rated 10 Desktop PCs available to buy right now: February 2018|2018/02/top-10-desktop-pcs-of-february-2018-buying-guide|AZ_51VaxxZYf8L",
--- en/0/la/laptops.js
+++ en/0/la/laptops.js
@@ -1,4 +1,3 @@
 Gr = [
 "Buying guide: top 10 Briefcases of March 2018|The best 10 Briefcases of March 2018: buying guide|2018/03/buying-guide-top-10-briefcases-of-march-2018|AZ_512FcoN4T7L",
-"Top-rated 10 Men's Razors of February 2018|The very best 10 Men's Razors of February 2018: buying guide|2018/02/top-rated-10-men-s-razors-of-february-2018|AZ_41b9oCBorcL",
 ];
