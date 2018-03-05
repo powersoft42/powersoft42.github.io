36a72df090af39c25b051610342c656fdf5680cb
microstorage <microstorage@users.noreply.github.com>
2018-03-05 18:49:44 +0100

    My commit

--- it/0/ar/aria-aperta.js
+++ it/0/ar/aria-aperta.js
@@ -1,3 +1,4 @@
 Gr = [
+"I 10 Zaini da Escursionismo preferiti: la classifica di marzo 2018|I 10 Zaini da Escursionismo più interessanti per l'acquisto|2018/03/i-10-zaini-da-escursionismo-preferiti-la-classifica-di-marzo-2018|AZ_41oB3GJI45L",
 "La top 10 dei migliori Binocoli: febbraio 2018|I 10 Binocoli più interessanti da valutare: la classifica|2018/02/la-top-10-dei-migliori-binocoli-febbraio-2018|AZ_41zW1mlAAQL",
 ];
--- it/0/index.js
+++ it/0/index.js
@@ -1,4 +1,5 @@
 Gr = [
+"I 10 Zaini da Escursionismo preferiti: la classifica di marzo 2018|I 10 Zaini da Escursionismo più interessanti per l'acquisto|2018/03/i-10-zaini-da-escursionismo-preferiti-la-classifica-di-marzo-2018|AZ_41oB3GJI45L",
 "Le 10 Tastiere PC migliori: marzo 2018|La top 10 delle migliori Tastiere PC sul mercato a marzo 2018|2018/03/le-10-tastiere-pc-migliori-marzo-2018|AZ_41wgHR31YbL",
 "I 9 Sci da Uomo al top: febbraio 2018|Sci da Uomo: i 9 migliori in commercio a febbraio 2018|2018/02/i-9-sci-da-uomo-al-top-febbraio-2018|AZ_41Bjafz6ElL",
 "Macchine da Caffè a Capsule: le 10 migliori a febbraio 2018|Le 10 Macchine da Caffè a Capsule che incontrano il maggiore gradimento|2018/02/macchine-da-caffe-a-capsule-le-10-migliori-a-febbraio-2018|AZ_41kpFIglQGL",
