f91ce4e3db58eb588143e461619cf1ffaaf7c730
microstorage <microstorage@users.noreply.github.com>
2018-03-18 22:05:26 +0100

    My commit

--- fr/0/bu/bureau.js
+++ fr/0/bu/bureau.js
@@ -1,3 +1,4 @@
 Gr = [
+"Les 10 meilleurs PC Portables de mars 2018|Découvrez les 10 meilleurs PC Portables de mars 2018|2018/03/les-10-meilleurs-pc-portables-de-mars-2018|AZ_41FNGBRQPPL",
 "Top 10 des meilleurs Ventilateurs Colonne de février 2018|Guide d'achat: les 10 meilleurs Ventilateurs Colonne du moment|2018/02/top-10-des-meilleurs-ventilateurs-colonne-de-fevrier-2018|AZ_31syhMT1qaL",
 ];
---
+++ fr/0/he/hewlett-packard.js
@@ -0,0 +1,3 @@
+Gr = [
+"Les 10 meilleurs PC Portables de mars 2018|Découvrez les 10 meilleurs PC Portables de mars 2018|2018/03/les-10-meilleurs-pc-portables-de-mars-2018|AZ_41FNGBRQPPL",
+];
