ed1edba415c955378605ef87a1085b3c2f0e57f4
microstorage <microstorage@users.noreply.github.com>
2018-04-05 16:12:24 +0200

    My commit

--- de/0/cl/clarks.js
+++ de/0/cl/clarks.js
@@ -1,4 +1,3 @@
 Gr = [
 "Top 10: die beliebtesten Artikel in Espadrilles für Herren, April 2018|10 beliebtesten Artikel in Espadrilles für Herren im Vergleich|2018/04/top-10-die-beliebtesten-artikel-in-espadrilles-fur-herren-april-2018|AZ_41Rcu6gblJL",
-"Top 10 Bestseller in Damen Sportschuhe: März 2018|Top 10: die beliebtesten Artikel in Damen Sportschuhe, März 2018|2018/03/top-10-bestseller-in-damen-sportschuhe-marz-2018|AZ_513hqW6QpqL",
 ];
