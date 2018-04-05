2eddb33462d08f1205e9af3dd34768995d3ed119
microstorage <microstorage@users.noreply.github.com>
2018-04-05 15:50:08 +0200

    My commit

--- fr/0/index.js
+++ fr/0/index.js
@@ -1,4 +1,5 @@
 Gr = [
+"Lunettes de Soleil Homme, les 10 meilleures: avril 2018|Le top 10 des meilleures Lunettes de Soleil Homme de avril 2018|2018/04/lunettes-de-soleil-homme-les-10-meilleures-avril-2018|AZ_41qNMYvKZNL",
 "Les 10 meilleurs Ordinateurs Portables Gamer de mars 2018|Découvrez les 10 meilleurs Ordinateurs Portables Gamer de mars 2018|2018/03/les-10-meilleurs-ordinateurs-portables-gamer-de-mars-2018|AZ_41sAvMeVWyL",
 "Les 10 meilleurs PC Portables de mars 2018|Découvrez les 10 meilleurs PC Portables de mars 2018|2018/03/les-10-meilleurs-pc-portables-de-mars-2018|AZ_41FNGBRQPPL",
 "Ventilateurs de Carte Graphique, les 10 meilleurs: mars 2018|Découvrez les 10 meilleurs Ventilateurs de Carte Graphique|2018/03/ventilateurs-de-carte-graphique-les-10-meilleurs-mars-2018|AZ_41srxaf4OLL",
---
+++ fr/0/lu/lunettes-soleil-homme.js
@@ -0,0 +1,3 @@
+Gr = [
+"Lunettes de Soleil Homme, les 10 meilleures: avril 2018|Le top 10 des meilleures Lunettes de Soleil Homme de avril 2018|2018/04/lunettes-de-soleil-homme-les-10-meilleures-avril-2018|AZ_41qNMYvKZNL",
+];
