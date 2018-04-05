7f755525bb2b4a224f8960d129fd959528ab3eb6
microstorage <microstorage@users.noreply.github.com>
2018-04-05 15:41:31 +0200

    My commit

--- es/0/an/android.js
+++ es/0/an/android.js
@@ -1,3 +1,4 @@
 Gr = [
+"Cámaras de Vigilancia, las 10 más valoradas: abril 2018|Las 10 más valoradas Cámaras de Vigilancia que puedes comprar|2018/04/camaras-de-vigilancia-las-10-mas-valoradas-abril-2018|AZ_41pT18qGesL",
 "Los 10 más valorados Tablets del momento: marzo 2018|La lista con los 10 Tablets más valorados a marzo 2018|2018/03/los-10-mas-valorados-tablets-del-momento-marzo-2018|AZ_41ApfygIgaL",
 ];
--- es/0/fu/full-hd.js
+++ es/0/fu/full-hd.js
@@ -1,4 +1,3 @@
 Gr = [
 "Cámaras de Vigilancia, las 10 más valoradas: abril 2018|Las 10 más valoradas Cámaras de Vigilancia que puedes comprar|2018/04/camaras-de-vigilancia-las-10-mas-valoradas-abril-2018|AZ_41pT18qGesL",
-"Los 10 Teclados mejor valorados: marzo 2018|Nuestra selección de los 10 Teclados mejor valorados a marzo 2018|2018/03/los-10-teclados-mejor-valorados-marzo-2018|AZ_516bBdetybL",
 ];
