5372e8d2892fc55857ae8f69745d60d172c113a9
microstorage <microstorage@users.noreply.github.com>
2018-06-02 16:15:47 +0200

    My commit

--- it/0/st/stile.js
+++ it/0/st/stile.js
@@ -1,4 +1,5 @@
 Gr = [
+"La top 10 dei migliori Jeans da Uomo a giugno 2018|I 10 Jeans da Uomo più interessanti in assoluto: la guida all'acquisto|2018/06/la-top-10-dei-migliori-jeans-da-uomo-a-giugno-2018|AZ_41L8ZGHpSkL",
 "Il meglio dei Portascarpe: aprile 2018|La top 10 dei migliori Portascarpe valutati a aprile 2018|2018/04/il-meglio-dei-portascarpe-aprile-2018|AZ_51C82Mu08jL",
 "La top 10 dei migliori Zoccoli e Sabot da Donna: aprile 2018|La top 10 dei migliori Zoccoli e Sabot da Donna in commercio|2018/04/la-top-10-dei-migliori-zoccoli-e-sabot-da-donna-aprile-2018|AZ_41yMqX-RSsL",
 ];
