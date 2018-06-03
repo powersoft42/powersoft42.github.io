43b1b78d49c661423f241884621ba155cc45d491
microstorage <microstorage@users.noreply.github.com>
2018-06-03 19:07:12 +0200

    My commit

--- fr/0/mo/mode.js
+++ fr/0/mo/mode.js
@@ -1,4 +1,5 @@
 Gr = [
+"Les 10 meilleurs Pantalons Homme de juin 2018|Notre sélection des 10 meilleurs Pantalons Homme du moment|2018/06/les-10-meilleurs-pantalons-homme-de-juin-2018|AZ_41KW6-p0HGL",
 "Jeans Homme, les 10 meilleurs: avril 2018|Le top 10 des meilleurs Jeans Homme de avril 2018|2018/04/jeans-homme-les-10-meilleurs-avril-2018|AZ_41h9PbgNlZL",
 "Lunettes de Soleil Homme, les 10 meilleures: avril 2018|Le top 10 des meilleures Lunettes de Soleil Homme de avril 2018|2018/04/lunettes-de-soleil-homme-les-10-meilleures-avril-2018|AZ_41qNMYvKZNL",
 ];
--- fr/0/ve/vetements-homme.js
+++ fr/0/ve/vetements-homme.js
@@ -1,3 +1,4 @@
 Gr = [
+"Les 10 meilleurs Pantalons Homme de juin 2018|Notre sélection des 10 meilleurs Pantalons Homme du moment|2018/06/les-10-meilleurs-pantalons-homme-de-juin-2018|AZ_41KW6-p0HGL",
 "Jeans Homme, les 10 meilleurs: avril 2018|Le top 10 des meilleurs Jeans Homme de avril 2018|2018/04/jeans-homme-les-10-meilleurs-avril-2018|AZ_41h9PbgNlZL",
 ];
