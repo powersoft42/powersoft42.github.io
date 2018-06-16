44382dba435374e9a5435b7e920caacbd97e1fdc
microstorage <microstorage@users.noreply.github.com>
2018-06-16 17:34:18 +0200

    My commit

--- fr/0/index.js
+++ fr/0/index.js
@@ -1,4 +1,5 @@
 Gr = [
+"Le top 10 des meilleurs Climatiseurs du moment: juin 2018|Notre sélection des 10 meilleurs Climatiseurs de juin 2018|2018/06/le-top-10-des-meilleurs-climatiseurs-du-moment-juin-2018|AZ_41kwrnrN9OL",
 "Les 10 meilleurs Pantalons Homme de juin 2018|Notre sélection des 10 meilleurs Pantalons Homme du moment|2018/06/les-10-meilleurs-pantalons-homme-de-juin-2018|AZ_41KW6-p0HGL",
 "Le top 10 des meilleures Smart TV du moment: juin 2018|Notre sélection des 10 meilleures Smart TV du moment: juin 2018|2018/06/le-top-10-des-meilleures-smart-tv-du-moment-juin-2018|AZ_51STl+QNTPL",
 "Jeans Homme, les 10 meilleurs: avril 2018|Le top 10 des meilleurs Jeans Homme de avril 2018|2018/04/jeans-homme-les-10-meilleurs-avril-2018|AZ_41h9PbgNlZL",
