b8657215144af882d575510017c58e30ba47a09b
microstorage <microstorage@users.noreply.github.com>
2018-06-30 17:29:03 +0200

    My commit

--- fr/0/fi/fitness.js
+++ fr/0/fi/fitness.js
@@ -1,3 +1,4 @@
 Gr = [
+"Découvrez les 10 meilleurs Appareils Abdominaux de juin 2018|Guide d'achat: les 10 meilleurs Appareils Abdominaux du moment|2018/06/decouvrez-les-10-meilleurs-appareils-abdominaux-de-juin-2018|AZ_513u0H-aoTL",
 "Electrostimulateurs, les 10 meilleurs: février 2018|Les 10 meilleurs Electrostimulateurs du moment: février 2018|2018/02/electrostimulateurs-les-10-meilleurs-fevrier-2018|AZ_510RfOYwzNL",
 ];
