bbc40c682e2637dd0a89594871a6c81638a55aa7
microstorage <microstorage@users.noreply.github.com>
2018-07-02 15:01:58 +0200

    My commit

--- de/0/cl/clatronic.js
+++ de/0/cl/clatronic.js
@@ -1,4 +1,5 @@
 Gr = [
+"Die 10 Bestseller in Standventilatoren: Juli 2018|10 beliebtesten Artikel in Standventilatoren zum Kaufen: Juli 2018|2018/07/die-10-bestseller-in-standventilatoren-juli-2018|AZ_41rSq4a9H8L",
 "Die 10 besten in Turmventilatoren: Juli 2018|Top 10: die 10 beliebtesten Artikel in Turmventilatoren, Juli 2018|2018/07/die-10-besten-in-turmventilatoren-juli-2018|AZ_31WxGa-8I9L",
 "10 besten in Wasserkocher zum Kaufen: März 2018|Top 10: die Bestseller in Wasserkocher, März 2018|2018/03/10-besten-in-wasserkocher-zum-kaufen-marz-2018|AZ_41WXogCV7iL",
 ];
--- de/0/el/elektrische-kuchengerate.js
+++ de/0/el/elektrische-kuchengerate.js
@@ -1,4 +1,5 @@
 Gr = [
+"Die 10 Bestseller in Standventilatoren: Juli 2018|10 beliebtesten Artikel in Standventilatoren zum Kaufen: Juli 2018|2018/07/die-10-bestseller-in-standventilatoren-juli-2018|AZ_41rSq4a9H8L",
 "Die 10 besten in Turmventilatoren: Juli 2018|Top 10: die 10 beliebtesten Artikel in Turmventilatoren, Juli 2018|2018/07/die-10-besten-in-turmventilatoren-juli-2018|AZ_31WxGa-8I9L",
 "10 Bestseller in Mini-Öfen, Juni 2018|Top 10 beliebtesten Artikel in Mini-Öfen: Juni 2018|2018/06/10-bestseller-in-mini-ofen-juni-2018|AZ_513sEi3tL3L",
 ];
