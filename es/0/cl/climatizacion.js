dffd54414229c5bf8ef4ccef3bc6715a4cfa3ef5
microstorage <microstorage@users.noreply.github.com>
2018-07-03 22:31:18 +0200

    My commit

--- es/0/cl/climatizacion.js
+++ es/0/cl/climatizacion.js
@@ -1,4 +1,5 @@
 Gr = [
+"Ventiladores para Ventanas, los 9 mejores del mercado: julio 2018|Nuestra selección de los 9 Ventiladores para Ventanas mejor valorados|2018/07/ventiladores-para-ventanas-los-9-mejores-del-mercado-julio-2018|AZ_51aHYpkbbqL",
 "El top 10 de los Ventiladores de Pie: julio 2018|Nuestra selección de los 10 Ventiladores de Pie mejor valorados|2018/07/el-top-10-de-los-ventiladores-de-pie-julio-2018|AZ_31-n6OCGkCL",
 "Los 10 Ventiladores de Sobremesa mejores a junio 2018|Los 10 más vendidos Ventiladores de Sobremesa del momento|2018/06/los-10-ventiladores-de-sobremesa-mejores-a-junio-2018|AZ_51OIBIFYajL",
 "Top 10 Radiadores de Aceite a marzo 2018|Los 10 mejor valorados en Radiadores de Aceite: marzo 2018|2018/03/top-10-radiadores-de-aceite-a-marzo-2018|AZ_51o-AjSOnmL",
--- es/0/ve/ventilador-ventana.js
+++ es/0/ve/ventilador-ventana.js
@@ -1,3 +1,4 @@
 Gr = [
+"Ventiladores para Ventanas, los 9 mejores del mercado: julio 2018|Nuestra selección de los 9 Ventiladores para Ventanas mejor valorados|2018/07/ventiladores-para-ventanas-los-9-mejores-del-mercado-julio-2018|AZ_51aHYpkbbqL",
 "Top 10 Ventiladores para Ventanas a febrero 2018|La lista con los 10 Ventiladores para Ventanas mejores a febrero 2018|2018/02/top-10-ventiladores-para-ventanas-a-febrero-2018|AZ_51aHYpkbbqL",
 ];
