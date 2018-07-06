79555a192798a80dc9caeaa7a442f7ef1520add6
microstorage <microstorage@users.noreply.github.com>
2018-07-06 15:54:04 +0200

    My commit

---
+++ en/0/fo/folding-bike.js
@@ -0,0 +1,3 @@
+Gr = [
+"The very best 10 Folding Bikes of July 2018|10 best-selling Folding Bikes of July 2018: buying guide|2018/07/the-very-best-10-folding-bikes-of-july-2018|AZ_51cGdA78aQL",
+];
--- en/0/index.js
+++ en/0/index.js
@@ -1,4 +1,5 @@
 Gr = [
+"The very best 10 Folding Bikes of July 2018|10 best-selling Folding Bikes of July 2018: buying guide|2018/07/the-very-best-10-folding-bikes-of-july-2018|AZ_51cGdA78aQL",
 "Our pick of the best 10 PC Fans: July 2018|PC Fans, the top 10 you may want to check out: July 2018|2018/07/our-pick-of-the-best-10-pc-fans-july-2018|AZ_51Xzmc+YHGL",
 "The very best 10 Ceiling Fans: July 2018|Best-selling 10 Ceiling Fans buyers guide of July 2018|2018/07/the-very-best-10-ceiling-fans-july-2018|AZ_319mKq6BZeL",
 "Top 10 Tower Fans of July 2018|Our pick of the best 10 Tower Fans: July 2018|2018/07/top-10-tower-fans-of-july-2018|AZ_41j74FTC5+L",
