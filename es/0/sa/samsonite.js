53f46c79f26cf87918284cb4bc008c417ab6c012
microstorage <microstorage@users.noreply.github.com>
2018-07-07 16:36:35 +0200

    My commit

--- es/0/sa/samsonite.js
+++ es/0/sa/samsonite.js
@@ -1,4 +1,3 @@
 Gr = [
 "Top 10 Equipaje de Mano a julio 2018|Equipaje de Mano, los 10 mejor valorados del mercado: julio 2018|2018/07/top-10-equipaje-de-mano-a-julio-2018|AZ_312zJRlfViL",
-"Los 10 Trolley más vendidos: julio 2018|La lista con los 10 Trolley más vendidos a julio 2018|2018/07/los-10-trolley-mas-vendidos-julio-2018|AZ_41xR6XQ5VeL",
 ];
