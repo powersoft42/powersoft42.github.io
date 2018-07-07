9546c50fc2ce7b2825fb60b2cd9fcc57b77aaf90
microstorage <microstorage@users.noreply.github.com>
2018-07-07 15:21:11 +0200

    My commit

--- it/0/mo/montagna.js
+++ it/0/mo/montagna.js
@@ -1,4 +1,5 @@
 Gr = [
+"Le 10 Tende a Tunnel più vendute: la classifica di luglio 2018|La top 10 delle migliori Tende a Tunnel disponibili da subito|2018/07/le-10-tende-a-tunnel-piu-vendute-la-classifica-di-luglio-2018|AZ_41bkgOufU4L",
 "Tende da Campeggio, le 10 consigliate: giugno 2018|Le 10 Tende da Campeggio che vanno di più subito disponibili|2018/06/tende-da-campeggio-le-10-consigliate-giugno-2018|AZ_41ixQ5LqATL",
 "I 10 Zaini da Escursionismo preferiti: la classifica di marzo 2018|I 10 Zaini da Escursionismo più interessanti per l'acquisto|2018/03/i-10-zaini-da-escursionismo-preferiti-la-classifica-di-marzo-2018|AZ_41oB3GJI45L",
 "I 9 Sci da Uomo al top: febbraio 2018|Sci da Uomo: i 9 migliori in commercio a febbraio 2018|2018/02/i-9-sci-da-uomo-al-top-febbraio-2018|AZ_41Bjafz6ElL",
--- it/0/te/tempo-libero.js
+++ it/0/te/tempo-libero.js
@@ -1,4 +1,5 @@
 Gr = [
+"Le 10 Tende a Tunnel più vendute: la classifica di luglio 2018|La top 10 delle migliori Tende a Tunnel disponibili da subito|2018/07/le-10-tende-a-tunnel-piu-vendute-la-classifica-di-luglio-2018|AZ_41bkgOufU4L",
 "Tende da Campeggio, le 10 consigliate: giugno 2018|Le 10 Tende da Campeggio che vanno di più subito disponibili|2018/06/tende-da-campeggio-le-10-consigliate-giugno-2018|AZ_41ixQ5LqATL",
 "I 10 Barbecue più amati: giugno 2018|La top 10 dei migliori Barbecue in commercio: giugno 2018|2018/06/i-10-barbecue-piu-amati-giugno-2018|AZ_41Vnxh6k8vL",
 "Compressori: i 10 migliori a aprile 2018|La top 10 dei migliori Compressori venduti a aprile 2018|2018/04/compressori-i-10-migliori-a-aprile-2018|AZ_51WhCtItZaL",
