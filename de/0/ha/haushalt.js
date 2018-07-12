6e4b9ea1f2f1335e2fd0edcff89412cfadca0c07
microstorage <microstorage@users.noreply.github.com>
2018-07-12 17:34:50 +0200

    My commit

--- de/0/ha/haushalt.js
+++ de/0/ha/haushalt.js
@@ -1,4 +1,5 @@
 Gr = [
+"Top 10: die 10 Bestseller in Sofa, Juli 2018|10 beliebtesten Artikel in Sofa zum Kaufen: Juli 2018|2018/07/top-10-die-10-bestseller-in-sofa-juli-2018|AZ_41iXjljXqgL",
 "Top 10 besten in Teebereiter: Juli 2018|Top 10: die 10 Bestseller in Teebereiter, Juli 2018|2018/07/top-10-besten-in-teebereiter-juli-2018|AZ_41DdPPmmA2L",
 "10 Bestseller in HeißWasserspender, Juli 2018|10 beliebtesten Artikel in HeißWasserspender zum Kaufen: Juli 2018|2018/07/10-bestseller-in-heisswasserspender-juli-2018|AZ_313ozvKDrAL",
 "Top 10 beliebtesten Artikel in Standmixer, Juli 2018|10 beliebtesten Artikel in Standmixer im Vergleich: Juli 2018|2018/07/top-10-beliebtesten-artikel-in-standmixer-juli-2018|AZ_41IzLKJD2FL",
---
+++ de/0/mo/mobel.js
@@ -0,0 +1,3 @@
+Gr = [
+"Top 10: die 10 Bestseller in Sofa, Juli 2018|10 beliebtesten Artikel in Sofa zum Kaufen: Juli 2018|2018/07/top-10-die-10-bestseller-in-sofa-juli-2018|AZ_41iXjljXqgL",
+];
