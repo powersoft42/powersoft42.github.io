0773d8203e1c5c26732692a506abf3ca9351f976
microstorage <microstorage@users.noreply.github.com>
2018-07-13 14:40:24 +0200

    My commit

--- de/0/lg/lg.js
+++ de/0/lg/lg.js
@@ -1,4 +1,3 @@
 Gr = [
 "Top 10 Bestseller in Waschmaschinen, Juli 2018|Top 10 beliebtesten Artikel in Waschmaschinen: Juli 2018|2018/07/top-10-bestseller-in-waschmaschinen-juli-2018|AZ_41dycMlzVeL",
-"10 besten in Geschirrspüler, April 2018|Top 10: die beliebtesten Artikel in Geschirrspüler, April 2018|2018/04/10-besten-in-geschirrspuler-april-2018|AZ_51Bi5RWruZL",
 ];
