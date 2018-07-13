4185045648c8acee7af940903a431c6690e18f1a
microstorage <microstorage@users.noreply.github.com>
2018-07-13 15:17:45 +0200

    My commit

--- es/0/bi/bicicletas-electricas.js
+++ es/0/bi/bicicletas-electricas.js
@@ -1,4 +1,3 @@
 Gr = [
 "Las 10 Bicicletas Eléctricas mejor valoradas a julio 2018|Nuestra selección de las 10 Bicicletas Eléctricas mejor valoradas|2018/07/las-10-bicicletas-electricas-mejor-valoradas-a-julio-2018|AZ_51mLT6ni0DL",
-"Las 10 mejor valoradas en Bicicletas de Montaña: marzo 2018|Nuestra selección de las 10 Bicicletas de Montaña mejor valoradas|2018/03/las-10-mejor-valoradas-en-bicicletas-de-montana-marzo-2018|AZ_51A8Ykal-CL",
 ];
--- es/0/index.js
+++ es/0/index.js
@@ -1,4 +1,5 @@
 Gr = [
+"Las 10 Bicicletas Eléctricas mejor valoradas a julio 2018|Nuestra selección de las 10 Bicicletas Eléctricas mejor valoradas|2018/07/las-10-bicicletas-electricas-mejor-valoradas-a-julio-2018|AZ_51mLT6ni0DL",
 "Top 10 Pantalones para Hombre a julio 2018|Nuestra selección de los 10 Pantalones para Hombre más valorados|2018/07/top-10-pantalones-para-hombre-a-julio-2018|AZ_41BDta2RZeL",
 "Drones, los 10 mejor valorados del mercado: julio 2018|Los 10 más valorados Drones que puedes comprar ahora mismo|2018/07/drones-los-10-mejor-valorados-del-mercado-julio-2018|AZ_31kd4fmepBL",
 "La lista con los 10 Pantalones Deportivos para Mujer mejores a julio 2018|Los 10 mejor valorados Pantalones Deportivos para Mujer del momento|2018/07/la-lista-con-los-10-pantalones-deportivos-para-mujer-mejores-a-julio-2018|AZ_415IoAwH6OL",
