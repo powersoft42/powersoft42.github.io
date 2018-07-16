feb6b2cbb47d8175a34d56cb0385f67f20e2e4d6
microstorage <microstorage@users.noreply.github.com>
2018-07-16 15:18:39 +0200

    My commit

--- en/0/du/duronic.js
+++ en/0/du/duronic.js
@@ -1,4 +1,3 @@
 Gr = [
 "Top 10 Portable Radios: July 2018|Most popular 10 Portable Radios of July 2018|2018/07/top-10-portable-radios-july-2018|AZ_41tRH5cbobL",
-"Top 10 Dj Mixers of February 2018|Our pick of the best 10 Dj Mixers: February 2018|2018/02/top-10-dj-mixers-of-february-2018|AZ_51faAw8-EiL",
 ];
