baa0440bda76519258e944176fea84f4702e5d99
microstorage <microstorage@users.noreply.github.com>
2018-07-21 15:02:50 +0200

    My commit

--- fr/0/je/jeux.js
+++ fr/0/je/jeux.js
@@ -1,4 +1,3 @@
 Gr = [
 "Guide d'achat: les 10 meilleurs Skateboards de juillet 2018|Notre sélection des 10 meilleurs Skateboards de juillet 2018|2018/07/guide-d-achat-les-10-meilleurs-skateboards-de-juillet-2018|AZ_51aL9lKSsmL",
-"Gps Auto, les 10 meilleurs: mars 2018|Guide d'achat: les 10 meilleurs Gps Auto du moment: mars 2018|2018/03/gps-auto-les-10-meilleurs-mars-2018|AZ_51UpRnL6bVL",
 ];
--- fr/0/sp/sports.js
+++ fr/0/sp/sports.js
@@ -1,4 +1,5 @@
 Gr = [
+"Guide d'achat: les 10 meilleurs Skateboards de juillet 2018|Notre sélection des 10 meilleurs Skateboards de juillet 2018|2018/07/guide-d-achat-les-10-meilleurs-skateboards-de-juillet-2018|AZ_51aL9lKSsmL",
 "Les 10 meilleurs Trampolines de juillet 2018|Top 10 des meilleurs Trampolines du moment: juillet 2018|2018/07/les-10-meilleurs-trampolines-de-juillet-2018|AZ_41z9W24-nQL",
 "Trackers D'Activité, les 10 meilleurs: juillet 2018|Découvrez les 10 meilleurs Trackers D'Activité de juillet 2018|2018/07/trackers-d-activite-les-10-meilleurs-juillet-2018|AZ_51HtIY2R8qL",
 "Découvrez les 10 meilleurs Vélos d'Appartement de juillet 2018|Guide d'achat: les 10 meilleurs Vélos d'Appartement de juillet 2018|2018/07/decouvrez-les-10-meilleurs-velos-d-appartement-de-juillet-2018|AZ_41Y5wsMeicL",
