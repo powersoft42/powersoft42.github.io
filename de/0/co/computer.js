11a6b78f5de1e0b87e7b8122372174a0f65abd89
microstorage <microstorage@users.noreply.github.com>
2018-07-23 16:37:18 +0200

    My commit

--- de/0/co/computer.js
+++ de/0/co/computer.js
@@ -1,4 +1,5 @@
 Gr = [
+"10 besten Notebooks im Vergleich: Juli 2018|Top 10 beliebtesten Artikel in Notebooks: Juli 2018|2018/07/10-besten-notebooks-im-vergleich-juli-2018|AZ_41ASL8fcMTL",
 "Top 10 besten USB-Ventilatoren: Juli 2018|10 besten USB-Ventilatoren zum Kaufen: Juli 2018|2018/07/top-10-besten-usb-ventilatoren-juli-2018|AZ_510K7z-0AUL",
 "Top 10 Bestseller in Gaming Notebooks: Juni 2018|Top 10: die beliebtesten Artikel in Gaming Notebooks, Juni 2018|2018/06/top-10-bestseller-in-gaming-notebooks-juni-2018|AZ_41NJWHWdIGL",
 "10 besten in Office-PCs im Vergleich: März 2018|Top 10: die 10 beliebtesten Artikel in Office-PCs, März 2018|2018/03/10-besten-in-office-pcs-im-vergleich-marz-2018|AZ_51rRkIE09PL",
--- de/0/hp/hp.js
+++ de/0/hp/hp.js
@@ -1,3 +1,4 @@
 Gr = [
+"10 besten Notebooks im Vergleich: Juli 2018|Top 10 beliebtesten Artikel in Notebooks: Juli 2018|2018/07/10-besten-notebooks-im-vergleich-juli-2018|AZ_41ASL8fcMTL",
 "Top 10 Bestseller in Gaming Notebooks: Juni 2018|Top 10: die beliebtesten Artikel in Gaming Notebooks, Juni 2018|2018/06/top-10-bestseller-in-gaming-notebooks-juni-2018|AZ_41NJWHWdIGL",
 ];
