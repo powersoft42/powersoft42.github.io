e8aa856d53eaf771e0007631f0d50fcf2479a94d
microstorage <microstorage@users.noreply.github.com>
2018-07-24 14:59:14 +0200

    My commit

--- de/0/de/dell.js
+++ de/0/de/dell.js
@@ -1,4 +1,5 @@
 Gr = [
+"Top 10 besten in Gaming Notebooks: Juli 2018|Top 10: die 10 beliebtesten Artikel in Gaming Notebooks, Juli 2018|2018/07/top-10-besten-in-gaming-notebooks-juli-2018|AZ_41u9j3WwiyL",
 "10 besten Notebooks im Vergleich: Juli 2018|Top 10 beliebtesten Artikel in Notebooks: Juli 2018|2018/07/10-besten-notebooks-im-vergleich-juli-2018|AZ_41ASL8fcMTL",
 "Top 10 Bestseller in Gaming Notebooks: Juni 2018|Top 10: die beliebtesten Artikel in Gaming Notebooks, Juni 2018|2018/06/top-10-bestseller-in-gaming-notebooks-juni-2018|AZ_41NJWHWdIGL",
 ];
--- de/0/ga/gaming.js
+++ de/0/ga/gaming.js
@@ -1,4 +1,5 @@
 Gr = [
+"Top 10 besten in Gaming Notebooks: Juli 2018|Top 10: die 10 beliebtesten Artikel in Gaming Notebooks, Juli 2018|2018/07/top-10-besten-in-gaming-notebooks-juli-2018|AZ_41u9j3WwiyL",
 "Top 10 besten USB-Ventilatoren: Juli 2018|10 besten USB-Ventilatoren zum Kaufen: Juli 2018|2018/07/top-10-besten-usb-ventilatoren-juli-2018|AZ_510K7z-0AUL",
 "Top 10 Bestseller in Gaming Notebooks: Juni 2018|Top 10: die beliebtesten Artikel in Gaming Notebooks, Juni 2018|2018/06/top-10-bestseller-in-gaming-notebooks-juni-2018|AZ_41NJWHWdIGL",
 "Top 10 besten in Gaming-PCs: März 2018|10 Bestseller in Gaming-PCs im Vergleich: März 2018|2018/03/top-10-besten-in-gaming-pcs-marz-2018|AZ_51KSPTYQ9lL",
