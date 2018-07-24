bf9ae7954b3fe87efcf12f7fa7913f5a8bfe6a9f
microstorage <microstorage@users.noreply.github.com>
2018-07-24 15:14:25 +0200

    My commit

--- fr/0/index.js
+++ fr/0/index.js
@@ -1,4 +1,5 @@
 Gr = [
+"Découvrez les 10 meilleurs PC Portables Gamer de juillet 2018|Guide d'achat: les 10 meilleurs PC Portables Gamer de juillet 2018|2018/07/decouvrez-les-10-meilleurs-pc-portables-gamer-de-juillet-2018|AZ_3157lbw-BzL",
 "Découvrez les 10 meilleurs Ordinateurs Portables de juillet 2018|Notre sélection des 10 meilleurs Ordinateurs Portables de juillet 2018|2018/07/decouvrez-les-10-meilleurs-ordinateurs-portables-de-juillet-2018|AZ_31I6Zh47eeL",
 "Top 10 des meilleures Tables à Repasser de juillet 2018|Notre sélection des 10 meilleures Tables à Repasser de juillet 2018|2018/07/top-10-des-meilleures-tables-a-repasser-de-juillet-2018|AZ_31rKRJnMUsL",
 "Guide d'achat: les 10 meilleurs Skateboards de juillet 2018|Notre sélection des 10 meilleurs Skateboards de juillet 2018|2018/07/guide-d-achat-les-10-meilleurs-skateboards-de-juillet-2018|AZ_51aL9lKSsmL",
--- fr/0/pc/pc-gaming.js
+++ fr/0/pc/pc-gaming.js
@@ -1,4 +1,5 @@
 Gr = [
+"Découvrez les 10 meilleurs PC Portables Gamer de juillet 2018|Guide d'achat: les 10 meilleurs PC Portables Gamer de juillet 2018|2018/07/decouvrez-les-10-meilleurs-pc-portables-gamer-de-juillet-2018|AZ_3157lbw-BzL",
 "Découvrez les 10 meilleurs Ordinateurs Portables de juillet 2018|Notre sélection des 10 meilleurs Ordinateurs Portables de juillet 2018|2018/07/decouvrez-les-10-meilleurs-ordinateurs-portables-de-juillet-2018|AZ_31I6Zh47eeL",
 "Ordinateurs Portables Gamer, les 10 meilleurs: juin 2018|Top 10 des meilleurs Ordinateurs Portables Gamer du moment: juin 2018|2018/06/ordinateurs-portables-gamer-les-10-meilleurs-juin-2018|AZ_41IDBjUsZcL",
 "Les 10 meilleurs Ordinateurs Portables Gamer de mars 2018|Découvrez les 10 meilleurs Ordinateurs Portables Gamer de mars 2018|2018/03/les-10-meilleurs-ordinateurs-portables-gamer-de-mars-2018|AZ_41sAvMeVWyL",
