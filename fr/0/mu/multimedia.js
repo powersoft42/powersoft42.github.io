23c68408cef94e0e258d55dd183244efb07e29fb
microstorage <microstorage@users.noreply.github.com>
2018-08-08 15:51:50 +0200

    My commit

--- fr/0/mu/multimedia.js
+++ fr/0/mu/multimedia.js
@@ -1,4 +1,5 @@
 Gr = [
+"Les 10 meilleurs Scanners de Diapositives du moment: août 2018|Guide d'achat: les 10 meilleurs Scanners de Diapositives du moment|2018/08/les-10-meilleurs-scanners-de-diapositives-du-moment-aout-2018|AZ_41Zp8s4JzIL",
 "Les 10 meilleures Compacts Hybrides de juillet 2018|Notre sélection des 10 meilleures Compacts Hybrides du moment|2018/07/les-10-meilleures-compacts-hybrides-de-juillet-2018|AZ_41s6-eGzFzL",
 "Top 10 des meilleurs Vidéoprojecteurs du moment: juillet 2018|Découvrez les 10 meilleurs Vidéoprojecteurs du moment: juillet 2018|2018/07/top-10-des-meilleurs-videoprojecteurs-du-moment-juillet-2018|AZ_41P128jziSL",
 "Chaînes Hi-Fi, les 10 meilleures: juillet 2018|Le top 10 des meilleures Chaînes Hi-Fi de juillet 2018|2018/07/chaines-hi-fi-les-10-meilleures-juillet-2018|AZ_41KjQmvXhwL",
---
+++ fr/0/ro/rollei.js
@@ -0,0 +1,3 @@
+Gr = [
+"Les 10 meilleurs Scanners de Diapositives du moment: août 2018|Guide d'achat: les 10 meilleurs Scanners de Diapositives du moment|2018/08/les-10-meilleurs-scanners-de-diapositives-du-moment-aout-2018|AZ_41Zp8s4JzIL",
+];
