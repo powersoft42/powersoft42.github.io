39e41661a18b5a6cac18a57f1b2cf9e2dee00f2f
microstorage <microstorage@users.noreply.github.com>
2018-08-23 16:08:33 +0200

    My commit

--- de/0/fo/fotodrucker.js
+++ de/0/fo/fotodrucker.js
@@ -1,3 +1,4 @@
 Gr = [
+"Top 10 Bestseller in Fotodrucker: August 2018|10 Bestseller in Fotodrucker zum Kaufen: August 2018|2018/08/top-10-bestseller-in-fotodrucker-august-2018|AZ_41AWJrXPsUL",
 "Top 10 Bestseller in Tintenstrahldrucker: August 2018|10 Bestseller in Tintenstrahldrucker im Vergleich: August 2018|2018/08/top-10-bestseller-in-tintenstrahldrucker-august-2018|AZ_4138KlV5DuL",
 ];
--- de/0/wi/windows.js
+++ de/0/wi/windows.js
@@ -1,4 +1,5 @@
 Gr = [
+"Top 10 Bestseller in Fotodrucker: August 2018|10 Bestseller in Fotodrucker zum Kaufen: August 2018|2018/08/top-10-bestseller-in-fotodrucker-august-2018|AZ_41AWJrXPsUL",
 "10 besten Gaming Notebooks zum Kaufen: August 2018|Top 10: die 10 besten Gaming Notebooks, August 2018|2018/08/10-besten-gaming-notebooks-zum-kaufen-august-2018|AZ_419YKI2IjLL",
 "Die 10 besten Gaming-PCs: August 2018|Top 10: die besten in Gaming-PCs, August 2018|2018/08/die-10-besten-gaming-pcs-august-2018|AZ_41kz3DgZ7ML",
 "Top 10 Bestseller in Tintenstrahldrucker: August 2018|10 Bestseller in Tintenstrahldrucker im Vergleich: August 2018|2018/08/top-10-bestseller-in-tintenstrahldrucker-august-2018|AZ_4138KlV5DuL",
