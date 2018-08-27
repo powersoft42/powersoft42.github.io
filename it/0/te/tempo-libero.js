549c2358bbc62ff45fc15d988577b65a7e890a43
microstorage <microstorage@users.noreply.github.com>
2018-08-27 14:46:06 +0200

    My commit

--- it/0/te/tempo-libero.js
+++ it/0/te/tempo-libero.js
@@ -1,4 +1,5 @@
 Gr = [
+"Le 10 Videocamere Digitali più quotate: agosto 2018|La top 10 delle migliori Videocamere Digitali: agosto 2018|2018/08/le-10-videocamere-digitali-piu-quotate-agosto-2018|AZ_51dnSwAzXLL",
 "Il meglio dei Monopattini Elettrici: agosto 2018|Monopattini Elettrici, i 10 preferiti in commercio: agosto 2018|2018/08/il-meglio-dei-monopattini-elettrici-agosto-2018|AZ_31Q1IaeipqL",
 "Il meglio delle T-Shirt Running Donna: agosto 2018|T-Shirt Running Donna, le 10 più gettonate subito disponibili|2018/08/il-meglio-delle-t-shirt-running-donna-agosto-2018|AZ_41kaan6ZqQL",
 "Il meglio dei Caschi da Bici per Adulto: luglio 2018|Caschi da Bici per Adulto, i 10 più amati disponibili da subito|2018/07/il-meglio-dei-caschi-da-bici-per-adulto-luglio-2018|AZ_417CWalnGkL",
--- it/0/wi/wi-fi.js
+++ it/0/wi/wi-fi.js
@@ -1,4 +1,3 @@
 Gr = [
 "Le 10 Videocamere Digitali più quotate: agosto 2018|La top 10 delle migliori Videocamere Digitali: agosto 2018|2018/08/le-10-videocamere-digitali-piu-quotate-agosto-2018|AZ_51dnSwAzXLL",
-"Le 10 Centrifughe che più piacciono: luglio 2018|La top 10 delle migliori Centrifughe in commercio a luglio 2018|2018/07/le-10-centrifughe-che-piu-piacciono-luglio-2018|AZ_31HyWwheEqL",
 ];
