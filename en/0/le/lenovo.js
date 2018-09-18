fc62d423ed651b469fa7cac4ac6c65325e177c29
microstorage <microstorage@users.noreply.github.com>
2018-09-18 21:15:31 +0200

    My commit

--- en/0/le/lenovo.js
+++ en/0/le/lenovo.js
@@ -1,4 +1,5 @@
 Gr = [
+"Best-selling 10 Portable Computers of September 2018|Our pick of the best 10 Portable Computers|2018/09/best-selling-10-portable-computers-of-september-2018|AZ_31GZXAIeWwL",
 "Top best 10 Desktop Computers to check out: July 2018|The very best 10 Desktop Computers buyers guide of July 2018|2018/07/top-best-10-desktop-computers-to-check-out-july-2018|AZ_41em-EltLBL",
 "Top 10 Desktop PCs of February 2018: buying guide|Top-rated 10 Desktop PCs available to buy right now: February 2018|2018/02/top-10-desktop-pcs-of-february-2018-buying-guide|AZ_51VaxxZYf8L",
 ];
--- en/0/po/portable-pc.js
+++ en/0/po/portable-pc.js
@@ -1,4 +1,3 @@
 Gr = [
 "Best-selling 10 Portable Computers of September 2018|Our pick of the best 10 Portable Computers|2018/09/best-selling-10-portable-computers-of-september-2018|AZ_31GZXAIeWwL",
-"The best 10 Laptop Chargers of September 2018|Laptop Chargers, the 10 best-sellers you may want to consider|2018/09/the-best-10-laptop-chargers-of-september-2018|AZ_41B-qR4jTWL",
 ];
