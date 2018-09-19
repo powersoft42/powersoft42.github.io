7f1e430fa335fb41973cd86368cc496cdb43bf75
microstorage <microstorage@users.noreply.github.com>
2018-09-19 15:42:28 +0200

    My commit

--- fr/0/or/ordinateurs-fixes.js
+++ fr/0/or/ordinateurs-fixes.js
@@ -1,4 +1,5 @@
 Gr = [
+"Top 10 des meilleurs Ordinateurs Gamer de septembre 2018|Notre sélection des 10 meilleurs Ordinateurs Gamer de septembre 2018|2018/09/top-10-des-meilleurs-ordinateurs-gamer-de-septembre-2018|AZ_41kz3DgZ7ML",
 "Top 10 des meilleurs Disques Durs Internes de septembre 2018|Découvrez les 10 meilleurs Disques Durs Internes du moment|2018/09/top-10-des-meilleurs-disques-durs-internes-de-septembre-2018|AZ_51VWD3poGeL",
 "Le top 10 des meilleurs PC Gamer de août 2018|Top 10 des meilleurs PC Gamer du moment: août 2018|2018/08/le-top-10-des-meilleurs-pc-gamer-de-aout-2018|AZ_512QvHkOeFL",
 "Souris Gaming, les 10 meilleures: août 2018|Guide d'achat: les 10 meilleures Souris Gaming de août 2018|2018/08/souris-gaming-les-10-meilleures-aout-2018|AZ_41HnXlM0i-L",
