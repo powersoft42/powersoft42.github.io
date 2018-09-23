ed2c46cad8b28b880f9f159861fb77177d15f2cf
microstorage <microstorage@users.noreply.github.com>
2018-09-23 15:59:19 +0200

    My commit

--- it/0/ip/iphone.js
+++ it/0/ip/iphone.js
@@ -1,3 +1,3 @@
 Gr = [
-"Un nuovo operatore operatore virtuale di telefonia mobile|Plintron si appoggerà alle reti Vodafone fino al 4G|2018/09/un-nuovo-operatore-operatore-virtuale-di-telefonia-mobile|smartphone-woman",
+"Un nuovo operatore virtuale di telefonia mobile|Plintron si appoggerà alle reti Vodafone fino al 4G|2018/09/un-nuovo-operatore-virtuale-di-telefonia-mobile|smartphone-woman",
 ];
