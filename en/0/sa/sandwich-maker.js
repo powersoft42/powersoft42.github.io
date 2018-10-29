c61015147b88c1bc5d6903136355148899823255
microstorage <microstorage@users.noreply.github.com>
2018-10-29 18:26:36 +0100

    My commit

--- en/0/sa/sandwich-maker.js
+++ en/0/sa/sandwich-maker.js
@@ -1,4 +1,3 @@
 Gr = [
 "The best 10 Sandwich Makers buyers guide of October 2018|Sandwich Makers, top best 10 you need to consider: October 2018|2018/10/the-best-10-sandwich-makers-buyers-guide-of-october-2018|AZ_51QcI-1T4PL",
-"Top best 10 Panini Presses of August 2018: buying guide|Buying guide: the best 10 Panini Presses of August 2018|2018/08/top-best-10-panini-presses-of-august-2018-buying-guide|AZ_416CYHZKNIL",
 ];
